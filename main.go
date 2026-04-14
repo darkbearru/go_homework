b5fc18722993705d71b3eb7c73edb9a90e856661
Alexey Abramenko <a.abramenko@chita.ru>
2026-04-14 13:02:37 +0300

    Add readAmount and calculateAmount functions

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const rateUSD2EUR = 1.14
 const rateUSD2RUB = 69.00
 const rateEUR2RUB = rateUSD2RUB / rateUSD2EUR
@@ -7,7 +9,18 @@ const rateEUR2RUB = rateUSD2RUB / rateUSD2EUR
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	var amountEUR float64 = 100
-	rateEUR2RUB := amountEUR * rateEUR2RUB
-	println(rateEUR2RUB)
+	var amountEUR float64 = readAmount()
+	amount := amountEUR * rateEUR2RUB
+	amount = calculateAmount(amount, "EUR", "RUB")
+	println(amount)
+}
+
+func readAmount() float64 {
+	var amount float64
+	fmt.Scan(&amount)
+	return amount
+}
+
+func calculateAmount(amount float64, currencySrc string, currencyDst string) float64 {
+	return amount
 }
