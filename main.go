2ad6818c598e802460f409ba94e5f810b85b88d7
Alexey Abramenko <a.abramenko@chita.ru>
2026-04-18 11:18:07 +0300

    Refactor currency handling: replace global `currencies` map with localized `CurrencyType`, enhance `userCurrencyInput` to utilize pointers, and improve formatting in output.

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ const rateUSD2EUR = 1.14
 const rateUSD2RUB = 69.00
 const rateEUR2RUB = rateUSD2RUB / rateUSD2EUR
 
-var currencies = map[string]int{"USD": 1, "EUR": 1, "RUB": 1}
 var ratesMap = map[string]float64{
 	"USD2RUB": rateUSD2RUB,
 	"USD2EUR": rateUSD2EUR,
@@ -19,24 +18,28 @@ var ratesMap = map[string]float64{
 	"RUB2EUR": 1 / rateEUR2RUB,
 }
 
+type CurrencyType = map[string]int
+
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
+	var currencies = CurrencyType{"USD": 1, "EUR": 1, "RUB": 1}
+
 	var currencySrc string = ""
 	var currencyDst string = ""
 	var amount float64 = 0
-	currencySrc = userCurrencyInput("Enter source currency:", currencySrc)
-	currencyDst = userCurrencyInput("Enter destination currency:", currencySrc)
+	currencySrc = userCurrencyInput("Enter source currency:", &currencySrc, &currencies)
+	currencyDst = userCurrencyInput("Enter destination currency:", &currencySrc, &currencies)
 	amount = readAmount("Enter amount:")
-	fmt.Printf("Amount: %f %s\n", amount, currencySrc)
-	fmt.Printf("Converted amount: %f %s\n", calculateAmount(amount, currencySrc, currencyDst), currencyDst)
+	fmt.Printf("Amount: %.2f %s\n", amount, currencySrc)
+	fmt.Printf("Converted amount: %.2f %s\n", calculateAmount(amount, currencySrc, currencyDst), currencyDst)
 }
 
-func userCurrencyInput(message string, selected string) string {
+func userCurrencyInput(message string, selected *string, currencies *CurrencyType) string {
 	var value string
 	var acceptedCurrencies []string
-	for value := range currencies {
-		if value != selected {
+	for value := range *currencies {
+		if value != *selected {
 			acceptedCurrencies = append(acceptedCurrencies, value)
 		}
 	}
@@ -48,7 +51,7 @@ func userCurrencyInput(message string, selected string) string {
 			continue
 		}
 		value = strings.ToUpper(value)
-		_, ok := currencies[value]
+		_, ok := (*currencies)[value]
 		if !ok {
 			fmt.Println("...Invalid currency")
 			continue
@@ -73,5 +76,6 @@ func readAmount(message string) float64 {
 }
 
 func calculateAmount(amount float64, currencySrc string, currencyDst string) float64 {
+	fmt.Println(currencySrc + "2" + currencyDst)
 	return amount * ratesMap[currencySrc+"2"+currencyDst]
 }
